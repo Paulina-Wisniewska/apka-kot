3e0fcf09b2342731d1b300587703a2371c88b770
Paulina Wisniewska <paulina10346@gmail.com>
2024-08-07 18:47:18 +0200

    added your cats page

---
+++ main.js
@@ -0,0 +1,20 @@
+// const app = document.querySelector('#app');
+// const addCatForm = document.querySelector('#add-cat');
+
+// window.onload = () => {
+//     const cats = JSON.parse(localStorage.getItem("firstCat"));
+    
+//     app.appendChild(newDiv)
+// }
+
+// addCatForm.addEventListener('submit', (event) => {
+//     event.preventDefault();
+//     const data = new FormData(event.target);
+
+//     const dataObject = Object.fromEntries(data.entries());
+// 	console.log(dataObject);
+
+//     localStorage.setItem("firstCat", JSON.stringify(dataObject));
+    
+//     addCatForm.reset();
+// });
